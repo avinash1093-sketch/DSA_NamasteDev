6d87adce356040586d64f0cbeb011b2b7d0100fb
Sachidanand Tiwari <avinash.sachidanand-tiwari@capgemini.com>
2025-08-05 11:55:24 +0530

    update in prac file

--- prac/prac_1.js
+++ prac/prac_1.js
@@ -1,4 +1,5 @@
 console.log('NaN--->',typeof NaN); //number
+console.log('Infinity--->',typeof Infinity); //number
 console.log('null-->',typeof null); // object
 console.log('undefined---->',typeof undefined); //undefined 
 console.log(NaN == null); //false
@@ -6,13 +7,15 @@ console.log(NaN === null); //false
 console.log(NaN == undefined); //false 
 console.log(NaN === undefined); //false
 console.log(undefined === null); // false
-console.log(isNaN(4)); 
-console.log(isNaN('four')); 
-console.log(isNaN(null)); 
-console.log(isNaN(-1)); 
+console.log(isNaN(4));  //true
+console.log(isNaN('four')); //false
 console.log(50 == "50"); //true
 console.log(50 === '50'); //false
 console.log(10 + 2 + "9"); //129
+console.log(isNaN(null));  // false
+console.log(isNaN(-1)); // false
+console.log(isNaN(Infinity));//false
+
 
 
 
